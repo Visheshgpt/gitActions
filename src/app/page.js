a4a095c8b796beaa163f235588b4fe185819d3cb
vishesh <guptavishesh98@gmail.com>
2024-02-04 11:31:18 +0530

    pr workflow check

--- src/app/page.js
+++ src/app/page.js
@@ -69,9 +69,9 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          {/* <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
+          </p> */}
         </a>
 
         <a
